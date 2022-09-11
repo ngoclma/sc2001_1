244e5c4ba979aaf49eabc84247a7d9a339aa530d
Jia Wei <kohjiawei1@gmail.com>
2022-09-11 19:40:42 +0800

    New InsertSort And MergeSort for fun

--- InsertSort.cpp
+++ InsertSort.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 
 void insertSort(int arr[],int arrSize);
+void mergeSort(int arr[], int firstIndex,int lastIndex);
+void merge(int arr[], int firstIndex,int lastIndex);
+void printArray(int arr[],int arrSize);
+
 
 int main() {
-    std::cout << "Hello World!\n";
-    int arr[] = {10,90,30,40,50,60};
+    //test arr
+    int arr[] = {12, 31, 25, 8, 32, 17, 40, 42};
+    //get size of array
     int arrSize = sizeof(arr)/sizeof(arr[0]);
+    //call inserSort function
     insertSort(arr,arrSize);
+   // mergeSort(arr,0,arrSize-1);
+    printArray(arr,arrSize);
+    return 0;
+}
+
+void printArray(int arr[],int arrSize){
     for(int i = 0 ; i< arrSize ; i++){
         printf("%d ",arr[i]);
     }
 }
+
 void insertSort(int arr[],int arrSize){
         int i,j; //using sizeof in function doesnt work
         int temp;
@@ -27,3 +40,61 @@ void insertSort(int arr[],int arrSize){
             }
         }
     }
+
+void mergeSort(int arr[], int firstIndex,int lastIndex){
+    
+    int mid = (firstIndex+lastIndex)/2;
+    if(lastIndex - firstIndex <= 0 ){
+        return;
+        }
+    else /*if(lastIndex - firstIndex > 1)*/{
+
+        mergeSort(arr, firstIndex, mid);
+        mergeSort(arr, mid+1, lastIndex);
+    }
+    merge(arr, firstIndex, lastIndex);
+}
+
+
+void merge(int arr[], int firstIndex,int lastIndex){
+    int mid = (firstIndex + lastIndex)/2;
+    int i,j;
+    //creating temp array
+    int leftSize = mid - firstIndex + 1;
+    int rightSize = lastIndex - mid;
+    
+    int leftArr[leftSize];
+    int rightArr[rightSize];   
+    //populating array
+    for(i = 0;i<leftSize;i++){
+        leftArr[i] = arr[i+firstIndex];
+    }
+    for(j = 0;j<rightSize;j++){
+        rightArr[j] = arr[j+mid+1];
+    }
+    i =0,j=0;
+    int x = firstIndex;
+    while(i<leftSize && j<rightSize ){
+        if(leftArr[i] <= rightArr[j]){
+            arr[x] = leftArr[i];
+            i++;
+            
+        } 
+        else{
+            arr[x] = rightArr[j];
+            j++;
+        }
+        x++;
+    }
+    //this is after either one of the subarr ends, we populate the remaining sorted array
+    while(i<leftSize){
+        arr[x] = leftArr[i];
+        i++;
+        x++;
+    }
+    while(i<leftSize){
+        arr[x] = rightArr[j];
+        j++;
+        x++;
+    }
+}
