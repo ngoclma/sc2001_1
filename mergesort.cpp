9db5989cfcba3ad9c9b544b4c3f1d34b3a68146b
Sherylleong <36541309+Sherylleong@users.noreply.github.com>
2022-09-12 21:23:33 +0800

    Create mergesort.cpp
    with inplace array

---
+++ mergesort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void mergesort(int arr[], int l, int r){
+    int mid = (l + r) / 2;
+    if (l <= r) return; // one or zero elements
+    else if (r - l > 1){ // array can be split further
+        mergesort(arr, l, mid);
+        mergesort(arr, mid+1, r);
+    }
+    merge(arr, l, r);
+}
+
+void merge(int arr[], int l, int r){
+    int mid = (l + r) / 2;
+    int a = l, b = mid+1, i, temp; // a and b are the first index of the subarrays
+    if (l < r) return;
+    while (a < mid && b < r){
+        if (arr[a] > arr[b]){
+            temp = arr[b++];
+            for (i = ++mid; i > a; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[a++] = temp;
+        }
+        else if (arr[a] < arr[b]) a++;
+        else {
+            if (a == mid && b == r) break;
+            temp = arr[b++];
+            a++;
+            for (i = ++mid, i > a, i--) arr[i] = arr[i-1];
+            arr[a++] = temp;
+        }
+    }
+}
