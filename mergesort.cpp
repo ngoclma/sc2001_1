7b9c0569678cd5b0268f6213c73065699dd96781
Sherylleong <36541309+Sherylleong@users.noreply.github.com>
2022-09-12 21:30:23 +0800

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 
-void mergesort(int arr[], int l, int r){
-    int mid = (l + r) / 2;
-    if (l <= r) return; // one or zero elements
-    else if (r - l > 1){ // array can be split further
-        mergesort(arr, l, mid);
-        mergesort(arr, mid+1, r);
-    }
-    merge(arr, l, r);
-}
-
 void merge(int arr[], int l, int r){
     int mid = (l + r) / 2;
     int a = l, b = mid+1, i, temp; // a and b are the first index of the subarrays
@@ -27,8 +17,18 @@ void merge(int arr[], int l, int r){
             if (a == mid && b == r) break;
             temp = arr[b++];
             a++;
-            for (i = ++mid, i > a, i--) arr[i] = arr[i-1];
+            for (i = ++mid; i > a; i--) arr[i] = arr[i-1];
             arr[a++] = temp;
         }
     }
+}
+
+void mergesort(int arr[], int l, int r){
+    int mid = (l + r) / 2;
+    if (l <= r) return; // one or zero elements
+    else if (r - l > 1){ // array can be split further
+        mergesort(arr, l, mid);
+        mergesort(arr, mid+1, r);
+    }
+    merge(arr, l, r);
 }
