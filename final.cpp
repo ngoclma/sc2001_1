06d32fd15f017bbab7fd8bedffe4507897bd9743
anhngoc <ngoc2162003@gmail.com>
2022-09-13 15:47:08 +0800

    nearly final

--- final.cpp
+++ final.cpp
@@ -8,19 +8,19 @@ using namespace std::chrono;
 // Declare S
 int S = 10;
 
-// The return types of sort methods are interger to save the number of key comparison
-int insertSort(int arr[], int arrSize, int firstIndex, int lastIndex);
-int mergeSort(int arr[], int arrSize, int firstIndex, int lastIndex);
-int mergeInsertSort(int arr[], int arrSize, int firstIndex, int lastIndex);
-int merge(int arr[], int arrSize, int firstIndex, int lastIndex);
+// The return types of sort methods are integer to save the number of key comparison
+int insertSort(int arr[], int firstIndex, int lastIndex);
+int mergeSort(int arr[], int firstIndex, int lastIndex);
+int mergeInsertSort(int arr[], int firstIndex, int lastIndex);
+int merge(int arr[], int firstIndex, int lastIndex);
 void printArray(int arr[], int arrSize);
 
 // Insertion Sort
-void insertSort(int arr[], int arrSize, int firstIndex, int lastIndex)
+int insertSort(int arr[], int firstIndex, int lastIndex)
 {
   int i, j, temp;
-  int count = 0;
-  for (int i = firstIndex; i < lastIndex; i++)
+  int c = 0;
+  for (int i = firstIndex; i <= lastIndex; i++)
   {
     for (int j = i; j > 0; j--)
     {
@@ -29,7 +29,7 @@ void insertSort(int arr[], int arrSize, int firstIndex, int lastIndex)
         temp = arr[j];
         arr[j] = arr[j - 1];
         arr[j - 1] = temp;
-        count++;
+        c++;
       }
       else
       {
@@ -37,11 +37,11 @@ void insertSort(int arr[], int arrSize, int firstIndex, int lastIndex)
       }
     }
   }
-  return count;
+  return c;
 }
 
 // Merge Sort
-int mergesort(int arr[], int l, int r)
+int mergeSort(int arr[], int l, int r)
 {
   int c = 0, c1 = 0, c2 = 0, c3 = 0;
   int mid = (l + r) / 2;
@@ -51,8 +51,8 @@ int mergesort(int arr[], int l, int r)
   } // one or zero elements
   else if (r - l > 1)
   { // array can be split further
-    c1 = mergesort(arr, l, mid);
-    c2 = mergesort(arr, mid + 1, r);
+    c1 = mergeSort(arr, l, mid);
+    c2 = mergeSort(arr, mid + 1, r);
   }
   c3 = merge(arr, l, r);
   c = c1 + c2 + c3;
@@ -64,8 +64,7 @@ int merge(int arr[], int l, int r)
   int c = 0;
 
   int mid = (l + r) / 2;
-  int a = l, b = mid + 1, i,
-      temp; // a and b are the first index of the subarrays
+  int a = l, b = mid + 1, i, temp;
   if (r <= l)
   {
     return 0;
@@ -108,7 +107,7 @@ int merge(int arr[], int l, int r)
 }
 
 // MergeInsert Sort
-void mergeInsertSort(int arr[], int arrSize, int firstIndex, int lastIndex)
+int mergeInsertSort(int arr[], int firstIndex, int lastIndex)
 {
   int c = 0, c1 = 0, c2 = 0, c3 = 0;
   int mid = (firstIndex + lastIndex) / 2;
@@ -119,19 +118,19 @@ void mergeInsertSort(int arr[], int arrSize, int firstIndex, int lastIndex)
   // Use merge sort if size of each sub-array is larger than S
   else if (lastIndex - firstIndex > 2 * S)
   {
-    c1 = mergeSort(arr, arrSize, firstIndex, mid);
-    c2 = mergeSort(arr, arrSize, mid + 1, lastIndex);
+    c1 = mergeSort(arr, firstIndex, mid);
+    c2 = mergeSort(arr, mid + 1, lastIndex);
   }
   // Use insertion sort if size of each sub-array is equal or smaller than S
   else
   {
-    c1 = insertSort(arr, arrSize, firstIndex, mid);
-    c2 = insertSort(arr, arrSize, mid + 1, lastIndex);
+    c1 = insertSort(arr, firstIndex, mid);
+    c2 = insertSort(arr, mid + 1, lastIndex);
   }
   c3 = merge(arr, firstIndex, lastIndex);
 
-  count = count1 + count2;
-  return count;
+  c = c1 + c2 + c3;
+  return c;
 }
 
 // Print & Check
@@ -142,7 +141,8 @@ void printArray(int arr[], int arrSize)
   {
     if ((i > 0) && (arr[i] < arr[i - 1]))
     {
-      printf("\n ERROR!!! \n") return;
+      printf("\n ERROR!!! \n");
+      return;
     }
     else
       printf("%d ", arr[i]);
@@ -153,33 +153,66 @@ void printArray(int arr[], int arrSize)
 int main()
 {
   // PLOTTING - vary array length (will count the number of key comparisons for plotting)
+  srand(unsigned(time(NULL)));
   long start = 1000;
-  long end = 1100;
-  int keyCompare[100];
-  int count = 0;
-  for (int n = start; n < end; n++)
+  long end = 2000;
+  int keyCompareInsert[1000], keyCompareMerge[1000], keyCompareInMe[1000];
+  for (int j = 0; j < 1000; j++)
+    keyCompareInsert[j] = keyCompareMerge[j] = keyCompareInMe[j] = 0;
+
+  int i = 0;
+  for (int n = start; n < end; n += 100)
   {
     // Generate random array of size n
-    int a[n];
+    int a[n], b[n], c[n];
+    // printf("Array size %d: ", n);
     for (int j = 0; j < n; j++)
+    {
       a[j] = rand() % 1000;
+      c[j] = b[j] = a[j];
+      // printf("%d,", c[j]);
+    }
+    // printf("\n");
 
     // --SORT--
     // TIMING - start
     // auto start = high_resolution_clock::now();
-    keyCompare[count++] = mergeSort(a, n, 0, n - 1);
+
+    keyCompareInsert[i] = insertSort(a, 0, n - 1);
+    keyCompareMerge[i] = mergeSort(b, 0, n - 1);
+    keyCompareInMe[i++] = mergeInsertSort(c, 0, n - 1);
+
+    /* printf("New a: ");
+    for (int j = 0; j < i; j++)
+        printf("%d,", a[j]);
+    printf("\n");
+    printf("New b: ");
+    for (int j = 0; j < i; j++)
+        printf("%d,", b[j]);
+    printf("\n");
+    printf("New c: ");
+    for (int j = 0; j < i; j++)
+        printf("%d,", c[j]);
+    printf("\n");*/
+
     // TIMING - end
     // auto stop = high_resolution_clock::now();
     // End timing, measure the duration
     // auto duration = duration_cast<microseconds>(stop - start);
     // printf("InsertSort took %f Seconds!\n",duration.count());
-
-    // Print & Check
-    // printArray(arr, arrSize);
   }
 
-  for (int i = 0; i < 100; i++)
-    print("%d,", keyCompare[i]);
+  printf("Insertion Sort:       ");
+  for (int j = 0; j < i; j++)
+    printf("%d,", keyCompareInsert[j]);
+  printf("\n");
+  printf("Merge Sort:           ");
+  for (int j = 0; j < i; j++)
+    printf("%d,", keyCompareMerge[j]);
+  printf("\n");
+  printf("Insertion Merge Sort: ");
+  for (int j = 0; j < i; j++)
+    printf("%d,", keyCompareInMe[j]);
 
   return 0;
 }
